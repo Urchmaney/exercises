8589213b76a49b7a7ac39063b362d2a909e94378
Unegbu Kingsley <kingsobino@gmail.com>
2023-12-12 16:57:18 +0100

    add frequency code from tree

--- compressor/test/compressor_test.go
+++ compressor/test/compressor_test.go
@@ -1,7 +1,6 @@
 package compressor_test
 
 import (
-	"fmt"
 	"testing"
 
 	compressor "../../../go-exercises/compressor"
@@ -11,7 +10,7 @@ func TestFrequencyCounter(t *testing.T) {
 
 	t.Run("Calculate Frequency", func(t *testing.T) {
 		data := "aaabbbc"
-		result := make(map[rune]int)
+		result := make(map[rune]*compressor.CharFreq)
 
 		if len(result) != 0 {
 			t.Errorf("Expected 0 but got %v", len(result))
@@ -29,13 +28,29 @@ func TestFrequencyCounter(t *testing.T) {
 
 func TestGenerateHuffmanBinaryTreeFromFrequency(t *testing.T) {
 	t.Run("Minimal", func(t *testing.T) {
-		data := map[rune]int{'C': 32, 'D': 42, 'E': 120, 'K': 7, 'L': 42, 'M': 24, 'U': 37, 'Z': 2}
+		data := map[rune]*compressor.CharFreq{
+			'C': &compressor.CharFreq{Freq: 32, Code: ""},
+			'D': &compressor.CharFreq{Freq: 42, Code: ""},
+			'E': &compressor.CharFreq{Freq: 120, Code: ""},
+			'K': &compressor.CharFreq{Freq: 7, Code: ""},
+			'L': &compressor.CharFreq{Freq: 42, Code: ""},
+			'M': &compressor.CharFreq{Freq: 24, Code: ""},
+			'U': &compressor.CharFreq{Freq: 37, Code: ""},
+			'Z': &compressor.CharFreq{Freq: 2, Code: ""}}
 		expected := 306
 
 		result := compressor.GenerateHuffmanBinaryTreeFromFrequency(data)
-		fmt.Println(result)
-		if *result.val > expected {
-			t.Errorf("Expected %v but got %v", expected, 2)
+		if result.Val != expected {
+			t.Errorf("Expected %v but got %v", expected, result.Val)
 		}
+
+		compressor.AddPrefixCode(data, result)
+		for _, v := range data {
+			if len(v.Code) == 0 {
+				t.Errorf("Expected length to be greather than 1 but got %v", v.Code)
+			}
+			// fmt.Printf("%v     %v\n", string(k), v.Code)
+		}
+
 	})
 }
