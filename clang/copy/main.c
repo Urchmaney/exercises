28200d49c9f34f55d43bfec9caeaf650fb539e0f
urchmaney <kingsobino@gmail.com>
2024-05-11 18:17:46 +0100

    add code

---
+++ clang/copy/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 2
+
+int main() {
+    int c, state;
+    state = OUT;
+    while((c = getchar()) != EOF) {
+        if(c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if(state == OUT) {
+            state = IN;
+            putchar('\n');
+        }
+        putchar(c);
+    }
+}
